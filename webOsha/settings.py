bac31fb2b89d661f394a2848d2910645c239918b
Shome Associates <shome.associates@hazlett.com.au>
2022-01-30 05:04:21 +0700

    update

--- webOsha/settings.py
+++ webOsha/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
-    'robots',
     'pengalamanKontakForm',
     'sertifikasi',
     'import_export',
