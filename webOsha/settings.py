0c348f9e91948b0b7c6e741ae2b134ac0f98b71e
Shome Associates <shome.associates@hazlett.com.au>
2022-01-30 12:35:08 +0700

    update

--- webOsha/settings.py
+++ webOsha/settings.py
@@ -64,9 +64,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
-
 PREPEND_WWW = False
 
 
